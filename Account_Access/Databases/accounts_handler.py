c7c7f5edfa3babdabd1167a0c7de389c7ea8eb6f
abhijay-py <jay62831@gmail.com>
2023-05-20 20:50:21 -0500

    modified the location of email_checker and clarified error states.

--- Account_Access/Databases/accounts_handler.py
+++ Account_Access/Databases/accounts_handler.py
@@ -27,6 +27,11 @@ def valid_password(password):
     
     return True 
 
+#Returns whether an email is valid.
+def valid_email(email):
+    regex = "[A-Za-z0-9\+-~_]+@[A-Za-z0-9\+-~_]+\.[A-Z|a-z]{2,7}"
+    return re.fullmatch(regex, email)
+
 #Returns whether the login attempt was sucessful (bool)
 def login_success(conn, email, password):
     query = f"SELECT password FROM accounts WHERE email = {email};"
@@ -66,7 +71,7 @@ def login_attempt(conn, email, password):
 
     return (account_exists, login_status, data)
 
-#Returns (account_created (bool), account_exists (bool), id_exists (bool), invalid_acc_type (bool), valid_password (bool))
+#Returns (account_created (bool), account_exists (bool), id_exists (bool), valid_acc_type (bool), valid_email (bool), valid_password (bool))
 #Last three booleans may return None if previous checks fail first. If account_created is true, all are true.
 def create_account(conn, email, password, id, accountType):
     account_types = ['P', 'D', 'N', 'A']
@@ -74,7 +79,7 @@ def create_account(conn, email, password, id, accountType):
     account_exists = account_exists(conn, email)
     
     if not account_exists:
-        return (False, account_exists, None, None, None)
+        return (False, account_exists, None, None, None, None)
 
     public_id = accountType + str(id)
     query = f"SELECT publicID FROM accounts WHERE publicID = {public_id};"
@@ -83,23 +88,28 @@ def create_account(conn, email, password, id, accountType):
     id_exists = bool(data)
 
     if not id_exists:
-        return (False, account_exists, id_exists, None, None)
+        return (False, account_exists, id_exists, None, None, None)
 
     valid_acc_type = accountType in account_types
 
     if not valid_acc_type:
-        return (False, account_exists, id_exists, valid_acc_type, None)
+        return (False, account_exists, id_exists, valid_acc_type, None, None)
     
+    email_is_valid = valid_email(password)
+
+    if not email_is_valid:
+        return (False, account_exists, id_exists, valid_acc_type, email_is_valid, None)
+
     password_is_valid = valid_password(password)
 
     if not password_is_valid:
-        return (False, account_exists, id_exists, valid_acc_type, password_is_valid)
+        return (False, account_exists, id_exists, valid_acc_type, email_is_valid, password_is_valid)
 
     query = "INSERT INTO accounts (email, id, accountType, password) VALUES (?, ?, ?, ?);"
     values = (email, public_id, accountType, password)
     insert_data(conn, query, values)
 
-    return (True, account_exists, id_exists, valid_acc_type, password_is_valid)
+    return (True, account_exists, id_exists, valid_acc_type, email_is_valid, password_is_valid)
 
 #Return (password_resetted (bool), account_exists (bool), valid_password (bool))
 #Last boolean may return None if previous checks fail first. If password_resetted is true, all are true.
@@ -119,18 +129,19 @@ def change_password(conn, email, password):
     
     return (True, account_exists, password_is_valid)
 
-#Return (email_changed (bool), account_exists (bool), email_is_used (bool))
+#Return (email_changed (bool), account_exists (bool), email_is_used (bool), email_is_valid (bool))
 def change_email(conn, old_email, new_email):
     account_exists = account_exists(conn, old_email)
     email_is_used = account_exists(conn, new_email)
-    
-    if not account_exists or email_is_used:
-        return (False, account_exists, email_is_used)
+    email_is_valid = valid_email(new_email)
+
+    if not account_exists or email_is_used or not email_is_valid:
+        return (False, account_exists, email_is_used, email_is_valid)
 
     query = f"UPDATE accounts SET email = {new_email} WHERE email = {old_email};"
     update_data(conn, query)
     
-    return (True, account_exists, email_is_used)
+    return (True, account_exists, email_is_used, email_is_valid)
 
 #Returns account_deleted (bool)
 #Deletes an account based on email provided. If false, account does not exist.
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-#Menu System/Login System through main
+#Modify login, new_account, pre_login_menu, and indivdual class menus after MVP is achieved.
+#Also modify how error states are handled.
 
 from Account_Access.Databases.accounts_handler import login_attempt, create_account
 from Account_Access.Databases.database_handler import create_connection, database_initialization
@@ -8,11 +9,6 @@ from Account_Access.hospital import Admin
 from Account_Access.nurse import Nurse
 from getpass import getpass
 
-#TODO: Implement
-#Returns valid_email (bool)
-def email_checker(email):
-    pass
-
 #Returns logged_in_acc (Account)
 #Keeps asking for login info until you're logged in
 def login(conn):
@@ -21,9 +17,6 @@ def login(conn):
     while not logged_in:
         email = input("Please enter your email: ")
         password = getpass("Please enter your password: ")
-
-        if not email_checker(email):
-            print("Please enter a valid email address.")
         
         account_exists, login_success, public_id  = login_attempt(conn, email, password)
 
